2d79b879d358af7f963dd4baca1a34075311855a
Justinas Kairys <38762355+ibnzUK@users.noreply.github.com>
2023-03-20 17:05:00 +0000

    add: version, nftMap, nftGet, nftAdd

---
+++ index.js
@@ -0,0 +1,170 @@
+const axios = require('axios');
+
+//Version
+async function version(params) {
+  const url = 'https://api.offsetdata.com/graphql';
+  let fields = [];
+  if (!params || params.length == 0) {
+    fields = ['date', 'current', 'creator', 'company', 'status'];
+  } else {
+    fields = params.map((param) => `${param}\n`).join('');
+  }
+
+  const query = `query {
+        version {
+          ${fields}
+        }
+      }`;
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const response = await axios.post(url, { query }, config);
+    const { version } = response.data.data;
+
+    return version;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+//NFT Mapping
+async function nftMap(apikey, chain, tokenAddress, tokenId) {
+  const url = 'https://api.offsetdata.com/graphql';
+  const query = `
+    mutation {
+      nftMap(meta: {
+        apikey: "${apikey}"
+        chain: "${chain}"
+        tokenAddress: "${tokenAddress}"
+        tokenId: "${tokenId}"
+      }) {
+        status
+      }
+    }
+  `;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const response = await axios.post(url, { query }, config);
+    return response.data.data.nftMap;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+//NFT GET
+async function nftGet(apikey, chain, tokenAddress, tokenId, params) {
+  const url = 'https://api.offsetdata.com/graphql';
+  // Generate the fields to include in the GraphQL query based on the input params array
+  let fields = [];
+  if (!params || params.length == 0) {
+    fields = [
+      'status',
+      'chain',
+      'tokenAddress',
+      'tokenId',
+      'title',
+      'description',
+      'symbol',
+      'tokenType',
+      'tokenUri { raw gateway }',
+      'media { raw gateway thumbnail format }',
+      'attributes { value trait_type }',
+    ];
+  } else {
+    fields = params.map((param) => `${param}\n`).join('');
+  }
+
+  const query = `
+            query {
+              nftGet(meta: {
+                apikey: "${apikey}"
+                chain: "${chain}"
+                tokenAddress: "${tokenAddress}"
+                tokenId: "${tokenId}"
+              }) {
+                ${fields} 
+              }
+            }
+          `;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const response = await axios.post(url, { query }, config);
+    return response.data.data.nftGet;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+//NFT Get
+async function nftAdd(apikey, params, attributes) {
+  const url = 'https://api.offsetdata.com/graphql';
+  // Generate the fields to include in the GraphQL query based on the input params array
+  const attributesString = attributes
+    ? `[${attributes
+        .map(
+          (attr) => `{
+      value: "${attr.value}",
+      trait_type: "${attr.trait_type}"
+    }`
+        )
+        .join(', ')}]`
+    : '[]';
+
+  const query = `
+  mutation {
+    nftAdd(meta: {
+        apikey: "${apikey}"
+        chain: "${params.chain}"
+        tokenAddress: "${params.tokenAddress}"
+        tokenId: "${params.tokenId}"
+        title: "${params.title}"
+        description: "${params.description}"
+        symbol: "${params.symbol}"
+        tokenType: "ERC721"
+        tokenUri: {
+          raw: "${params.tokenUri.raw}",
+          gateway:"${params.tokenUri.gateway}",
+        }
+        media: {
+                raw: "${params.media.raw}",
+                gateway: "${params.media.gateway}",
+                thumbnail: "${params.media.thumbnail}",
+                format: "${params.media.format}"
+        }
+        attributes: ${attributesString}
+    }) {
+     status
+    }
+  }
+        `;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const response = await axios.post(url, { query }, config);
+    return response.data.data.nftAdd;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = {
+  version,
+  nftMap,
+  nftGet,
+  nftAdd,
+};
---
+++ test/script.js
@@ -0,0 +1,85 @@
+const offsetdata = require('offsetdata');
+
+// *** test Version
+
+//   offsetdata.version(["date", "current", "creator", "company", "status"])
+//   .then(object => {
+//     console.log(object);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });  
+
+// *** test NFT MAP
+
+// offsetdata.nftMap("KEY", "eth", "0xb334a4eb0a2d6cc24fd451e779c002b9b33228c3", "2650")
+//   .then(object => {
+//     console.log(object);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+// *** test NFT GET
+
+// offsetdata.nftGet("KEY", "eth", "0xb334a4eb0a2d6cc24fd451e779c002b9b33228c3", "2650", [
+//     "status",
+//     "chain",
+//     "tokenAddress",
+//     "tokenId",
+//     "title",
+//     "description",
+//     "symbol",
+//     "tokenType",
+//     "tokenUri { raw gateway }",
+//     "media { raw gateway thumbnail format }",
+//     "attributes { value trait_type }"
+// ])
+//   .then(object => {
+//     console.log(object);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+
+// test NFT ADD
+
+// offsetdata.nftAdd("KEY", {
+//     chain: "eth",
+//     tokenAddress: "0x152f04b930f9902471ddcba278c28bd51448fd11",
+//     tokenId: "9",
+//     title: "Justin Token",
+//     description: "This token is for asdakd adjadjk",
+//     symbol: "JST",
+//     tokenType: "ERC721",
+//     tokenUri: {
+//       raw: "token raw link",
+//       gateway:"token uri gatway link"
+//     },
+//     media: {
+// 			raw: "rawString",
+//   		gateway: "gatewayString",
+//       thumbnail: "thumbnailString",
+//       format: "formatString"
+//     }
+// }, 	[
+//     {
+//       value: "value1",
+//       trait_type: "trait1"
+//     },
+//     {
+//       value: "value2",
+//       trait_type: "trait2"
+//     },
+//     {
+//         value: "value3",
+//         trait_type: "trait3"
+//       },
+//   ])
+//   .then(object => {
+//     console.log(object);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
